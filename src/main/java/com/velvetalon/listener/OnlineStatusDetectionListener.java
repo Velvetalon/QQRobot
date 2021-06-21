2eed9121feec9b4e0f42251e98f9df35d111941d
Velvetalon <stardust.whc@gmail.com>
2021-06-21 14:41:47 +0800

    + 更新了版本号：1.2 + 增加快速检测机器人在线状态的指令。 + 更新了README文件。

---
+++ src/main/java/com/velvetalon/listener/OnlineStatusDetectionListener.java
@@ -0,0 +1,35 @@
+package com.velvetalon.listener;
+
+import com.velvetalon.utils.MessageUtil;
+import love.forte.simbot.annotation.Filter;
+import love.forte.simbot.annotation.OnGroup;
+import love.forte.simbot.annotation.Priority;
+import love.forte.simbot.api.message.MessageContentBuilder;
+import love.forte.simbot.api.message.events.GroupMsg;
+import love.forte.simbot.api.sender.MsgSender;
+import love.forte.simbot.api.sender.Sender;
+import org.springframework.stereotype.Component;
+
+/**
+ * @describe: 文件描述
+ * @author: Velvetalon
+ * HISTORY:
+ * <p>
+ * 2021/6/21 14:25 : 创建文件
+ */
+@Component
+public class OnlineStatusDetectionListener {
+    @OnGroup
+    @Filter("#还在吗")
+    public void func( GroupMsg groupMsg, MsgSender sender ){
+        MessageContentBuilder builder = MessageUtil.builder();
+        builder.face(17);
+        sender.SENDER.sendGroupMsg(groupMsg, builder.build());
+        builder = MessageUtil.builder();
+        builder.text("我在！");
+        sender.SENDER.sendGroupMsg(groupMsg, builder.build());
+        builder = MessageUtil.builder();
+        builder.imageLocal(this.getClass().getClassLoader().getResource("OnlineStatusResp.jpg").getPath());
+        sender.SENDER.sendGroupMsg(groupMsg, builder.build());
+    }
+}
